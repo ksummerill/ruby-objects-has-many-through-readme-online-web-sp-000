6f2b5a560892e3425d498f0087e8c6078dad61a2
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-12 19:58:43 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,7 @@
 class Customer
+
+  # Their name and age are set upon initialization, 
+  # and because we created an attribute accessor for both, the customer can change their name or age
   attr_accessor :name, :age
 
   @@all = []
